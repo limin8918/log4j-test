f4bf928fed8070c9b85a09ef416832db48375496
Min Li <minli@thoughtworks.com>
2014-12-12 17:12:49 +0800

    output the same class log into different log file

--- src/test/TestA.java
+++ src/test/TestA.java
@@ -3,9 +3,11 @@ package test;
 import org.apache.log4j.Logger;
 
 public class TestA {
-    private final Logger LOG = Logger.getLogger(getClass());
+    private final Logger LOG1 = Logger.getLogger("testOne");
+    private final Logger LOG2 = Logger.getLogger("testTwo");
 
     public void outputLog () {
-        LOG.debug("this is A class");
+        LOG1.debug("this is A class, should in log one");
+        LOG2.debug("this is A class, should in log two");
     }
 }
