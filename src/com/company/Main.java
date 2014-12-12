5c8bba72b727259a399a48482969c34abe7d782d
Min Li <minli@thoughtworks.com>
2014-12-12 14:05:37 +0800

    output the log

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,15 @@ public class Main {
     public static void main(String[] args) {
         PropertyConfigurator.configure("./config/log4j.properties");
 
+        TestA a = new TestA();
+        a.outputLog();
+
+        TestB b = new TestB();
+        b.outputLog();
+
+        TestC c = new TestC();
+        c.outputLog();
+
         System.out.print("finish");
     }
 }
