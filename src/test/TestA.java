f46028f35c84a36e9dfd92bccc2dc73b7fa19e92
Min Li <minli@thoughtworks.com>
2014-12-12 17:42:32 +0800

    log level test

--- src/test/TestA.java
+++ src/test/TestA.java
@@ -5,9 +5,12 @@ import org.apache.log4j.Logger;
 public class TestA {
     private final Logger LOG1 = Logger.getLogger("testOne");
     private final Logger LOG2 = Logger.getLogger("testTwo");
+    private final Logger LOG3 = Logger.getLogger("testThree");
 
     public void outputLog () {
         LOG1.debug("this is A class, should in log one");
         LOG2.debug("this is A class, should in log two");
+        LOG3.info("this is A class, the log level is error, so should not in log three");
+        LOG3.warn("this is A class, the log level is correct, so should in log three");
     }
 }
