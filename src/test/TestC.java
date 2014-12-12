1108267a5eba1fddb34ac292e947d9b55ad055f7
Min Li <minli@thoughtworks.com>
2014-12-12 16:58:21 +0800

    output different class log to different file

--- src/test/TestC.java
+++ src/test/TestC.java
@@ -3,7 +3,7 @@ package test;
 import org.apache.log4j.Logger;
 
 public class TestC {
-    private final Logger LOG = Logger.getLogger(getClass());
+    private final Logger LOG = Logger.getLogger("myLog");
 
     public void outputLog () {
         LOG.warn("this is C class");
