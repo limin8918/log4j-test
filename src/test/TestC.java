0a1d586f00f0a519112ab22e51253eadf728270b
Min Li <minli@thoughtworks.com>
2014-12-12 18:06:38 +0800

    output all log into one file

--- src/test/TestC.java
+++ src/test/TestC.java
@@ -3,7 +3,7 @@ package test;
 import org.apache.log4j.Logger;
 
 public class TestC {
-    private final Logger LOG = Logger.getLogger(getClass());
+    private final Logger LOG = Logger.getLogger("myProjectLog");
 
     public void outputLog () {
         LOG.warn("this is C class");
